49e5cf706b1d04f9f2965c95c6dde5e4dbc6c2f7
jacoby149 <jackstudent149@gmail.com>
2022-08-23 19:40:58 -0400

    working for google

--- api/app/main.py
+++ api/app/main.py
@@ -1,93 +1,80 @@
-import app.settings as settings
-import app.docs as docs
-import app.models as models
-import app.exceptions as exceptions
+# import app.settings as settings
+# import app.docs as docs
+# import app.models as models
+# import app.exceptions as exceptions
 
-# interfaces
-import app.mongo as db
-import app.twilio as mobile
-import app.stripe as pay
-
-
-#############################################
-########### APP INITIALIZATION ##############
-#############################################
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-app = FastAPI(
-    title="web10",
-    openapi_tags=docs.tags_metadata,
-    description=docs.description,
-    version="10.0.0.0",
-    terms_of_service="http://example.com/terms/",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-####################################################
-################## Handle 422s #####################
-####################################################
-
-import logging
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
-    logging.error(f"{request}: {exc_str}")
-    content = {"status_code": 10422, "message": exc_str, "data": None}
-    return JSONResponse(
-        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )
-
+# import app.mongo as db
+# import app.twilio as mobile
+# import app.stripe as pay
 
 ###################################
 ######## Github Oauth Flow ########
 ###################################
 
-from authlib.integrations.starlette_client import OAuth
+import json
+from fastapi import FastAPI
 from starlette.config import Config
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, RedirectResponse
+from authlib.integrations.starlette_client import OAuth, OAuthError
 
-config = Config('.env')  # read config from .env file
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="!secret")
+
+config = Config('.env')
 oauth = OAuth(config)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 oauth.register(
     name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    server_metadata_url=CONF_URL,
     client_kwargs={
         'scope': 'openid email profile'
     }
 )
 
-from fastapi import FastAPI
-from starlette.middleware.sessions import SessionMiddleware
 
-app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="secret-string")
+@app.get('/')
+async def homepage(request: Request):
+    user = request.session.get('user')
+    if user:
+        data = json.dumps(user)
+        html = (
+            f'<pre>{data}</pre>'
+            '<a href="/logout">logout</a>'
+        )
+        return HTMLResponse(html)
+    return HTMLResponse('<a href="/login">login</a>')
 
-@app.route('/login')
+
+@app.get('/login')
 async def login(request: Request):
-    # absolute url for callback
-    # we will define it below
     redirect_uri = request.url_for('auth')
     return await oauth.google.authorize_redirect(request, redirect_uri)
 
-@app.route('/auth')
-async def auth(request: Request):
-    token = await oauth.google.authorize_access_token(request)
-    # <=0.15
-    # user = await oauth.google.parse_id_token(request, token)
-    user = token['userinfo']
-    return user
 
+@app.get('/auth')
+async def auth(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as error:
+        return HTMLResponse(f'<h1>{error.error}</h1>')
+    user = token.get('userinfo')
+    if user:
+        request.session['user'] = dict(user)
+    return RedirectResponse(url='/')
+
+
+@app.get('/logout')
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000)
 
 ##################################
 ####### Ranking Promoting ########
